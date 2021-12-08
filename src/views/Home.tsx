48682cba729df983bd2856030712c317c32e8760
beastOP <omkarpatil290@gmail.com>
2021-12-08 23:21:49 +0530

    🥱Adding a component

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,20 @@
 import { Component } from 'solid-js';
 import { useAuth } from '@lib/Auth'
+import Counter from '@components/Counter'
 
 const Home: Component = () => {
 	const { isAuthed, login, logout } = useAuth()
     return (
-        <div class="p-4 flex justify-center items-center text-center">
+        <div class="p-4 flex justify-center text-center">
 		{
 			isAuthed() ?
-			<div>
-				<p>You're authenticated</p>
+			<div class="flex flex-col gap-4 items-center">
+				<p>You're authenticated!</p>
+				<Counter />
 				<button class="py-1 text-white px-6 bg-red-500 rounded" onclick={logout}>Logout</button>
 			</div>
 			: <div>
-				<p>You're unauthenticated</p>
+				<p>You're unauthenticated!</p>
 				<button class="py-1 text-white px-6 bg-blue-700 rounded" onclick={login}>Login</button>
 			</div>
 		}
